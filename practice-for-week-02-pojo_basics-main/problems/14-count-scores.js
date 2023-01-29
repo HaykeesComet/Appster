0dc80e98a0878c7a44abb35e1b6c704cb394f304
Hector Sevilla <haykees.comet@live.com>
2023-01-29 15:12:09 -0500

    Update.

--- practice-for-week-02-pojo_basics-main/problems/14-count-scores.js
+++ practice-for-week-02-pojo_basics-main/problems/14-count-scores.js
@@ -28,8 +28,38 @@ console.log(countScores(peeps)); //=> { Anthony: 4, Fred: 4, Winnie: 6 }
 ***********************************************************************/
 
 function countScores(people) {
-  // Your code here
+  let scoresObj = {};
+  let scores = 0;
+  for (let i = 0; i < people.length; i++) {
+    let valuesArr = Object.values(people[i]);
+    scores = valuesArr[1]
+    if (scoresObj[valuesArr[0]] === undefined) {
+      scoresObj[valuesArr[0]] = scores;
+    }
+    else {
+      scoresObj[valuesArr[0]] += scores;
+    }
+  }
+  return scoresObj;
 }
+
+let ppl = [{ name: "Anthony", score: 10 },
+{ name: "Fred", score: 10 },
+{ name: "Anthony", score: -8 },
+{ name: "Winnie", score: 12 }];
+
+console.log(countScores(ppl)); //=> { Anthony: 2, Fred: 10, Winnie: 12 }
+
+let peeps = [
+  { name: "Anthony", score: 2 },
+  { name: "Winnie", score: 2 },
+  { name: "Fred", score: 2 },
+  { name: "Winnie", score: 2 },
+  { name: "Fred", score: 2 },
+  { name: "Anthony", score: 2 },
+  { name: "Winnie", score: 2 }
+];
+console.log(countScores(peeps)); //=> { Anthony: 4, Fred: 4, Winnie: 6 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = countScores;
